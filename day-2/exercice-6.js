bbe9bbad54c0c6ed04d75bab9b9e579b3cd73ece
Arthur Roux <arthur.roux@epita.fr>
2021-10-20 16:15:23 +0200

    exo 6 day 2

--- day-2/exercice-6.js
+++ day-2/exercice-6.js
@@ -1,8 +1,20 @@
-const isCharAnAlpha = (char) => {
-    return (char >= '0' && char <= '9') || 
-    (char >= 'a' && char <= 'z') || char === ' ';
-}
-
 export const my_display_unicode = (arr) => {
-    
+    var i = 0;
+    var str = "";
+    let alpha = "abcdefghijklmnopqrstuvwxyz";
+    let alphaMAJ = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    while (arr[i] != undefined) {
+        let char = arr[i];
+        if (char >= 97 && char <= 122) {
+            str += alpha[char - 97];
+        } else if (char >= 48 && char <= 57) {
+            str += char - 48;
+        } else if (char >= 65 && char <= 90) {
+            str += alphaMAJ[char - 65];
+        } else if (char === 32) {
+            str += ' ';
+        }
+        i++;
+    }
+    return str;
 }
